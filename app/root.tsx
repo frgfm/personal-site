86541bbe4765ccfbdfc686954e3c44fc6a7d2fae
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2025-02-22 12:19:32 +0100

    style: fix format in jsx

--- app/root.tsx
+++ app/root.tsx
@@ -12,10 +12,10 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
